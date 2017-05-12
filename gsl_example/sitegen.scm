6eb5cbfb5617f1c77868be9beca84f3234dc8452
Jeff Mickey <j@codemac.net>
2017-05-12 09:57:42 -0700

    Further progress with local-eval

--- gsl_example/sitegen.scm
+++ gsl_example/sitegen.scm
@@ -1,9 +1,14 @@
-(for-ref-set! site.section.page
-	      (xml-read (read-ref page 'name #:as "~a.xml")))
+(use-modules (ice-9 eval-string))
+
+(for! site.section.page
+      (xml-read (read-ref page 'name #:as "~a.xml")))
 
 ;; now we should run the template! :/
-(for-ref site.section.page
-	 (format #t "page: ~s~%" page)
-	 (read-and-eval! (open-file "template.pop" "r")))
+(for site.section.page
+  (format #t "*wrapped-e*: ~s~%" *wrapped-e*)
+  (let* ((iport (open-file "template.pop" "r"))
+	 (exp (read-all iport)))
+    (local-eval exp *wrapped-e*)
+    (close-port iport)))
 
 (sxml-save! "root.sxml")
